ccad3bb990e112aec03977bc81a2159270c5653c
MaxWhite2024 <mjwhite@scu.edu>
2024-03-07 15:15:28 -0800

    basic enemy movement

--- Shadow Boxer/Assets/Scripts/Enemy.cs
+++ Shadow Boxer/Assets/Scripts/Enemy.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class Enemy : Destructable
 {
+    private enum Direction_Type
+    {
+        NONE, RIGHT, LEFT, TOWARDS, AWAY
+    }
+
+    //component vars
     private SpriteChanges child_sprite_changes;
     private SpriteEffects child_sprite_effects;
     private Transform proj_spawn_trans;
+
+    //Serialized vars
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float timeBetweenAttack = 0.5f;
     [SerializeField] public float attackDuration = 0.5f;
     [SerializeField] public float deathDuration = 1.5f; 
+    [SerializeField] private Direction_Type moveDirection = Direction_Type.NONE;
+    [SerializeField] private float moveSpeed = 0f;
+
+    //temp vars
     private float count = 0f;
+    private Vector3 vect_mov_dir = Vector3.zero;
+    private Transform trans;
+
+    private Vector3 Direction_Type_To_Vector(Direction_Type dir_type)
+    {
+        if(dir_type == Direction_Type.NONE)
+            return Vector3.zero;
+        else if(dir_type == Direction_Type.RIGHT)
+            return new Vector3(1,0,0);
+        else if(dir_type == Direction_Type.LEFT)
+            return new Vector3(-1,0,0);
+        else if(dir_type == Direction_Type.TOWARDS)
+            return new Vector3(0,0,-1);
+        else if(dir_type == Direction_Type.AWAY)
+            return new Vector3(0,0,1);
+        else
+            return Vector3.zero;
+    }
 
     void Start()
     {
         child_sprite_changes = gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteChanges>();
         child_sprite_effects = gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteEffects>();
         proj_spawn_trans = gameObject.transform.GetChild(1);
+        vect_mov_dir = Direction_Type_To_Vector(moveDirection);
+        trans = gameObject.transform; 
     }
 
     // Update is called once per frame
@@ -40,6 +72,9 @@ public class Enemy : Destructable
                 //reset count
                 count = 0f;
             }
+            
+            //move the sprite according to vect_mov_dir
+            trans.position += vect_mov_dir * moveSpeed * Time.deltaTime;
         }
     }
 
@@ -78,6 +113,9 @@ public class Enemy : Destructable
     {
         yield return new WaitForSeconds(wait_time);
 
-        Instantiate(projectilePrefab, proj_spawn_trans.position, Quaternion.identity);
+        if(health > 0)
+        {
+            Instantiate(projectilePrefab, proj_spawn_trans.position, Quaternion.identity);
+        }
     }
 }
